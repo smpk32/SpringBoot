e531ecfa96a07d39c2bbf4a74092a25cdf2d7bef
smpk32 <smpk32@naver.com>
2020-06-22 17:57:47 +0900

    쿼리메소드 연습

--- eclipse-workspace/Chapter05/src/main/java/com/sangmin/persistence/BoardRepository.java
+++ eclipse-workspace/Chapter05/src/main/java/com/sangmin/persistence/BoardRepository.java
@@ -1,11 +1,20 @@
 package com.sangmin.persistence;
 
+
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import com.sangmin.domain.Board;
 
 public interface BoardRepository extends CrudRepository<Board, Long> {
 	List<Board>	findByTitle(String searchKeyword);
+	List<Board> findByContentContaining(String searchKeyword);
+	List<Board> findByTitleContaining(String searchKeyword);
+	List<Board> findByTitleContainingOrContentContaining(String title,String content);
+	List<Board> findByTitleContainingOrderBySeqDesc(String searchKeyword);
+	Page<Board> findByTitleContaining(String searchKeyword,Pageable paging);
+	
 }
--- eclipse-workspace/Chapter05/src/test/java/com/sangmin/QueryMethodTest.java
+++ eclipse-workspace/Chapter05/src/test/java/com/sangmin/QueryMethodTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.sangmin.domain.Board;
@@ -19,27 +23,76 @@ class QueryMethodTest {
 	@Autowired
 	private BoardRepository boardRepo;
 	
-	@Before
-	public void dataPrepare() {
-		for (int i = 1; i <=200 ; i++) {
-			Board board = new Board();
-			board.setTitle("테스트 제목 "+i);
-			board.setWriter("테스터");
-			board.setContent("테스트 내용 : "+ i);
-			board.setCreateDate(new Date());
-			board.setCnt(0L);
-			boardRepo.save(board);
-		}
-		System.out.println("22222");
-	}
+//	@Before
+//	public void dataPrepare() {
+//		for (int i = 1; i <=200 ; i++) {
+//			Board board = new Board();
+//			board.setTitle("테스트 제목 "+i);
+//			board.setWriter("테스터");
+//			board.setContent("테스트 내용 : "+ i);
+//			board.setCreateDate(new Date());
+//			board.setCnt(0L);
+//			boardRepo.save(board);
+//		}
+//		System.out.println("22222");
+//	}
+//	
+//	@Test
+//	public void testFindByTitle() {
+//		List<Board> boardList = boardRepo.findByTitle("테스트 제목 10");
+//		System.out.println("검색 결과");
+//		for(Board board : boardList) {
+//			System.out.println("--->"+board.toString());
+//		}
+//		
+//	}
 	
+//	@Test
+//	public void testByContentContaning() {
+//		List<Board> boardList = boardRepo.findByTitleContaining("1");
+//		
+//		System.out.println("검색결과");
+//		for(Board board : boardList) {
+//			System.out.println("--->"+board.toString());
+//		}
+//	}
+	
+//	@Test
+//	public void testFindByTitleContainingOrContentContaining() {
+//		List<Board> boardList = boardRepo.findByTitleContainingOrContentContaining("17", "17");
+//		
+//		System.out.println("검색결과");
+//		for(Board board : boardList) {
+//			System.out.println("--->"+board.toString());
+//		}
+//	}
+	
+//	@Test
+//	public void testFindByTitleContainingOrderBySeqDesc() {
+//		List<Board> boardList = boardRepo.findByTitleContainingOrderBySeqDesc("17");
+//		
+//		System.out.println("검색결과");
+//		for(Board board:boardList) {
+//			System.out.println("--->"+board.toString());
+//		}
+//	}
 	@Test
-	public void testFindByTitle() {
-		List<Board> boardList = boardRepo.findByTitle("테스트 제목 10");
-		System.out.println("검색 결과");
-		for(Board board : boardList) {
+	public void testFindByTitleContaining() {
+		Pageable paging = PageRequest.of(0, 5,Sort.Direction.DESC,"seq");
+		Page<Board> pageInfo = boardRepo.findByTitleContaining("제목", paging);
+		
+		System.out.println("PAGE SIZE : "+pageInfo.getSize());
+		System.out.println("TOTAL PAGES : "+pageInfo.getTotalPages());
+		System.out.println("TOTAL COUNT : "+pageInfo.getTotalElements());
+		System.out.println("NEXT : "+pageInfo.nextPageable());
+		
+		List<Board> boardList = pageInfo.getContent();
+		System.out.println("검색결과");
+		for(Board board:boardList) {
 			System.out.println("--->"+board.toString());
 		}
+		
+		
 		
 	}
 
